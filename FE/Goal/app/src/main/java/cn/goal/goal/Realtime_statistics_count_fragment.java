7e5f9cac69ff5392eb8caf30d35a3a529bf387e4
976170778@qq.com <976170778@qq.com>
2017-02-17 22:23:56 +0800

    实时统计基本分页

---
+++ FE/Goal/app/src/main/java/cn/goal/goal/Realtime_statistics_count_fragment.java
@@ -0,0 +1,17 @@
+package cn.goal.goal;
+
+/**
+ * Created by 97617 on 2017/2/16.
+ */
+import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+public class Realtime_statistics_count_fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) { // TODO Auto-generated method stub
+        View view = inflater.inflate(R.layout.fragement_count_and_analyse, null);
+        return view;
+    }
+}

fb3de90e7f904319652923bbfff28c944029597b
976170778@qq.com <976170778@qq.com>
2017-02-21 23:41:38 +0800

    每一个目标点击之后的页面

---
+++ FE/Goal/app/src/main/java/cn/goal/goal/Goal_record_class.java
@@ -0,0 +1,59 @@
+package cn.goal.goal;
+
+/**
+ * Created by 97617 on 2017/2/21.
+ */
+
+public class Goal_record_class {
+    private int user_Image;
+    private String user_name;
+    private String user_record;
+    private String time_of_send;
+    private int ImageId_btn_like;
+    private int sum_of_like;
+    private int ImageId_btn_share;
+    private int ImageId_btn_reply;
+    public Goal_record_class(int user_Image,String user_name,String time_of_send,int sum_of_like)
+    {
+        this.user_Image=user_Image;
+        this.user_name=user_name;
+        this.user_record="我们诚惶诚恐，我们小心翼翼，我们毫无保留，却不一定就能把爱，变成相爱。如果这一次真得背过身我们不再遇见了，那我希望我们可以面对面再背对背，永远不留遗憾，一生山高水阔。只是，后来，我们都甘愿被生活凌迟。";
+        this.time_of_send=time_of_send;
+        this.ImageId_btn_like=R.mipmap.like;
+        this.sum_of_like=sum_of_like;
+        this.ImageId_btn_reply=R.mipmap.reply;
+        this.ImageId_btn_share=R.mipmap.share2;
+    }
+
+    /**
+     * get方法的实现
+     * @return
+     */
+    public int getUser_Image()
+    {
+        return user_Image;
+    }
+    public String getUser_name(){
+        return user_name;
+    }
+    public String getUser_record(){
+        return user_record;
+    }
+    public String getTime_of_send()
+    {
+        return time_of_send;
+    }
+    public int getImageId_btn_like(){
+        return ImageId_btn_like;
+    }
+    public int getSum_of_like(){
+        return sum_of_like;
+    }
+    public int getImageId_btn_share(){
+        return ImageId_btn_share;
+    }
+    public int getImageId_btn_reply(){
+        return ImageId_btn_reply;
+    }
+
+}
--- FE/Goal/app/src/main/java/cn/goal/goal/Realtime_FragmentAdapter.java
+++ FE/Goal/app/src/main/java/cn/goal/goal/Realtime_FragmentAdapter.java
@@ -5,26 +5,29 @@ package cn.goal.goal;
  */
 
 import java.util.List;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-public class Realtime_FragmentAdapter extends FragmentPagerAdapter
-{
+
+public class Realtime_FragmentAdapter extends FragmentPagerAdapter {
     private List<Fragment> fragmentList;
-    public Realtime_FragmentAdapter(FragmentManager fm, List<Fragment> fragmentList)
-    {
+
+    public Realtime_FragmentAdapter(FragmentManager fm, List<Fragment> fragmentList) {
         super(fm);
         // TODO Auto-generated constructor stub
         this.fragmentList = fragmentList;
     }
-    @Override
-    public Fragment getItem(int arg0)
-    { // TODO Auto-generated method stub
-        return fragmentList.get(arg0);
-    } @Override
-public int getCount()
-{ // TODO Auto-generated method stub
 
-    return fragmentList.size();
-}
+    @Override
+    public Fragment getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return fragmentList.get(arg0);
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return fragmentList.size();
+    }
 }

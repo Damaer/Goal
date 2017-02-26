9ce86a12e183daf501d00b2598857eb32480ed6b
Gentle_Lee <tommy007lee@gmail.com>
2017-02-26 22:08:38 +0800

    recommend test

--- FE/Goal/app/src/main/java/cn/goal/goal/RecommendFragment.java
+++ FE/Goal/app/src/main/java/cn/goal/goal/RecommendFragment.java
@@ -30,19 +30,12 @@ public class RecommendFragment extends Fragment {
 
         recommendListVIew = (ListView) view.findViewById(R.id.recommend_listView);
         createListView();
-        addListener();
         return view;
     }
 
     private void createListView(){
 
         recommendListAdapter = new RecommendAdapter(getActivity(),getData());
-//        recommendListAdapter = new SimpleAdapter(getContext(),
-//                getData(), R.layout.recommend_list_item,new String[] {"portrait","name","follow_number",
-//                "follow_button","share_text","like_button","share_button","like_number","follow_number","follow_add_button"},
-//                new int[] {R.id.portrait_img,R.id.name,R.id.follow_number,
-//                        R.id.follow_button,R.id.share_text,R.id.like_button,R.id.share_button,
-//                        R.id.like_number,R.id.follow_number});
 
         recommendListVIew.setAdapter(recommendListAdapter);
     }
@@ -52,34 +45,19 @@ public class RecommendFragment extends Fragment {
         dataList = new ArrayList<>();
         for (int i = 0;i<20;i++) {
             Map<String, Object> map = new HashMap<>();
+
             /*
-            * new 一个Recommend，然后添加到map里面去,把用户名称，头像，都放进去；
+            * 个人认为我new 一个Recommend，然后添加到map里面去,把用户名称，头像，都放进去；然后到Adapter类里面去get会不会比较好？
              */
            // Recommend data = new Recommend();
             map.put("name","name"+i);//getUserName获取用户姓名
             map.put("portrait",R.mipmap.ic_launcher);//getUserImg获取用户头像
             map.put("follow_number","2017-2-2");//getCreateAt获取用户时间
             map.put("follow_button","关注");
-            map.put("follow_add_button",R.drawable.ic_add_white_24dp);
             map.put("share_text","你熟悉爱辜负大家发的的李开复哈大姐夫监控卡两地分居漏电开关暗恋看到过暗地里看风景暗恋的看法");//getContent();获取内容
             dataList.add(map);
         }
         return dataList;
-    }
-    private  void  addListener(){
-//        followButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(follow == false){
-//                    followButton.setBackgroundColor(Integer.parseInt("#66BB6A"));
-//                    follow = true;
-//                }else{
-//                    followButton.setBackgroundColor(Integer.parseInt("#E8F5E9"));
-//                    followButton.setTextColor(Integer.parseInt("#E8F5E9"));
-//                    followButton.setText("已关注");
-//                }
-//            }
-//        });
     }
 
 }

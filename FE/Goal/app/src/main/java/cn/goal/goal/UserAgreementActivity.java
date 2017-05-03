b8691d0e05a45b5d4afdd9473396be0b5d7913db
976170778@qq.com <976170778@qq.com>
2017-05-03 17:12:42 +0800

    aword everyday

--- FE/Goal/app/src/main/java/cn/goal/goal/UserAgreementActivity.java
+++ FE/Goal/app/src/main/java/cn/goal/goal/UserAgreementActivity.java
@@ -9,9 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
-public class UserAgreementActivity extends AppCompatActivity {
-
+public class UserAgreementActivity extends AppCompatActivity implements View.OnClickListener {
+    public ImageButton comeback_from_useragreement;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,24 @@ public class UserAgreementActivity extends AppCompatActivity {
         Window window = getWindow();
         //设置透明状态栏,这样才能让 ContentView 向上
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+    }
+    public void init(){
+        comeback_from_useragreement= (ImageButton) findViewById(R.id.comeback_from_useragreement);
+        comeback_from_useragreement.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.comeback_from_useragreement:
+                finish();
+                break;
+            default:
+                break;
+        }
 
     }
 }

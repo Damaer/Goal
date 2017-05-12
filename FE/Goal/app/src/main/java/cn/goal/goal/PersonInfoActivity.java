8e397fa83a8037ae3130d270cbdf7adb3f0ceb50
Pencil <9807175@qq.com>
2017-05-12 23:48:59 +0800

    修改设置界面菜单背景颜色

--- FE/Goal/app/src/main/java/cn/goal/goal/PersonInfoActivity.java
+++ FE/Goal/app/src/main/java/cn/goal/goal/PersonInfoActivity.java
@@ -7,7 +7,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
